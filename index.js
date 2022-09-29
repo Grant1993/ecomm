ca036aa12347e0733c9902b5010dcb9d41dcfd5c
Grant <grant1993@live.com>
2022-09-29 18:31:41 +0100

    image and file upload

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/products');
 
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    cookieSession({
-        keys: ['fdgfdgertegdfggfdwe']
-    })
+  cookieSession({
+    keys: ['lkasld235j']
+  })
 );
-
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3001, () => {
-    console.log('Listening');
+  console.log('Listening');
 });
